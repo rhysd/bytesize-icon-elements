0c3543c9ca4a36d3e048e28e2a7a3a3a85abbe22
rhysd <lin90162@yahoo.co.jp>
2016-11-20 02:50:10 +0900

    use Ruby to generate README

--- scripts/generate_readme.js
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env node
-
-const list = require('../index.js').ICON_NAMES.map(n => `- \`<icon-${n}><icon-${n}/>\``).join('\n')
-const template = require('fs').readFileSync(require('path').join(__dirname, 'README_template.md'), 'utf8');
-process.stdout.write(template + list + '\n');
-
---
+++ scripts/readme_gen.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+icons = File.join(__dir__, '..', 'node_modules', 'bytesize-icons', 'dist/icons')
+template = File.join(__dir__, 'README_template.md')
+readme = File.join(__dir__, '..', 'README_template.md')
+list = Dir.glob(File.join(icons, '*.svg'))
+  .map{|f| File.basename(f, '.svg')}
+  .map{|n| "- `<icon-#{n}></icon-#{n}>`"}
+  .join("\n")
+puts(File.read(template) + list)
